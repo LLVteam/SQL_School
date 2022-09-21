8e7efe1a63344eee7836a7af14de9365983619a9
Lucia Muñoz Bohollo <94393718+lmunozbohollo@users.noreply.github.com>
2022-09-21 21:33:07 +0200

    Added trigger

--- advance_options.sql
+++ advance_options.sql
@@ -1,6 +1,6 @@
 USE School;
 
--- Prepare an example query with group by and having to demostrate how to extract data from your DB for analysis.
+-- 1. Prepare an example query with group by and having to demostrate how to extract data from your DB for analysis.
 
 -- Who are the students who have lunch at school?
 
@@ -8,8 +8,27 @@ SELECT
 	COUNT(DISTINCT s.name) As "School meal",
 	s.name As "Student name"
 FROM 
-	Student s
+	Students s
 GROUP BY 
 	s.name
 HAVING 
 	COUNT(s.school_meals) = 1;
+    
+
+-- 2. Create a trigger and demonstrate how it runs
+
+-- Students with over 3 penalties are expelled
+
+USE School;
+
+CREATE TRIGGER update_expulsions
+BEFORE UPDATE
+ON Students
+FOR EACH ROW
+SET @penalties = @penalties+1;
+
+UPDATE School.Students
+SET status = 'Expelled' 
+WHERE Student_ID=3 AND 8 AND 17;
+
+SELECT * FROM school.students;
